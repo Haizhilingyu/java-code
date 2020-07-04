ff8ecfbb0a57481006ce1a94a7fb210444740697
hai <haizhilingyu@gmail.com>
2020-07-04 21:44:36 +0800

    添加日志代码片段

---
+++ log/README.md
@@ -0,0 +1,5 @@
+## SysLogs.java
+    日志注解定义
+
+## SysLogAspect.java
+    日志Spring切片处理，存es，存文件等
---
+++ log/SysLogs.java
@@ -0,0 +1,11 @@
+import java.lang.annotation.*;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SysLogs {
+
+    String value();
+
+}
